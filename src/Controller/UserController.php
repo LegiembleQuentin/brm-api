57233ba94f1b3c5a6f54ff30a048694584a0dd9a
ArthurJfr <jaffro.arthur@gmail.com>
2023-11-28 23:37:42 +0100

    setPassword method

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -54,74 +54,68 @@ class UserController extends AbstractController
     {
             $decoded = json_decode($request->getContent());
 
-            $email = $decoded->email;
-            $username = $decoded->username;
-            $roles = $decoded->roles;
-            // enbaled = false par défaut -> passera à true quand le user aura set son password après la verif de son token
-            $enabled = 0; // = false
-            // Création du token à stocker en bdd // invitation_token
-            $token = bin2hex(random_bytes(32));
-            //Date d'expiration du token
-            $expiry = date_create_immutable('+1');
-            $expiry = $expiry->modify('+2 day');
-            $expiryToken = $expiry->format('Y-m-d H:i:s');
+            if (!empty($decoded))
+            {
+                $email = $decoded->email;
+                $username = $decoded->username;
+                $roles = $decoded->roles;
+                $enabled = 0; // = false
+                $token = bin2hex(random_bytes(32));
+                //Date d'expiration du token
+                $expiry = date_create_immutable('+1');
+                $expiry = $expiry->modify('+2 day');
+                //$expiryToken = $expiry->format('Y-m-d H:i:s');
+                $created_at = date_create_immutable();
+                $roleEmployee = $decoded->roleEmployee;
+                $sexe = $decoded->sexe;
+                $name = $decoded->name;
+                $fname = $decoded->fname;
+                $birthday = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->birth);
+                $hire_date = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->hire);
+                $phone = $decoded->phone;
+                $adress = $decoded->adress;
+                $postal = $decoded->postal;
+                $secuNum = $decoded->secuNum;
+                $contract = $decoded->contract;
+                $contractEnd = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->contractEnd);
+                $disability = $decoded->disability;
+                $disabilityDesc = $decoded->disabilityDesc;
+                $enabledEmployee = $enabled;
 
-            //$birtday = strtotime($decoded->birth);
+                $user = new User();
+                $employee = new Employee();
 
-            $created_at = date_create_immutable();
-            $roleEmployee = $decoded->roleEmployee;
-            $sexe = $decoded->sexe;
-            $name = $decoded->name;
-            $fname = $decoded->fname;
+                $employee->setEnabled($enabledEmployee);
+                $employee->setCreatedAt($created_at);
+                $employee->setUser($user->getId());
+                $employee->setAdress($adress);
+                $employee->setBirthdate($birthday);
+                $employee->setContractEndDate($contractEnd);
+                $employee->setContractType($contract);
+                $employee->setDisability($disability);
+                $employee->setDisabilityDesc($disabilityDesc);
+                $employee->setFirstname($fname);
+                $employee->setHireDate($hire_date);
+                $employee->setName($name);
+                $employee->setPhone($phone);
+                $employee->setSexe($sexe);
+                $employee->setPostalCode($postal);
+                $employee->setSocialSecurityNumber($secuNum);
+                $employee->setRole($roleEmployee);
 
-            $birthday = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->birth);
-            //$birthday = $birthday_at;
-
-            $hire_date = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->hire);
-            $phone = $decoded->phone;
-            $adress = $decoded->adress;
-            $postal = $decoded->postal;
-            $secuNum = $decoded->secuNum;
-            $contract = $decoded->contract;
-            $contractEnd = DateTime::createFromFormat('Y-m-d H:i:s', $decoded->contractEnd);
-            $disability = $decoded->disability;
-            $disabilityDesc = $decoded->disabilityDesc;
-            $enabledEmployee = $enabled;
-
-
-            $user = new User();
-            $employee = new Employee();
-
-            $employee->setEnabled($enabledEmployee);
-            $employee->setCreatedAt($created_at);
-            $employee->setUser($user->getId());
-            $employee->setAdress($adress);
-            $employee->setBirthdate($birthday);
-            $employee->setContractEndDate($contractEnd);
-            $employee->setContractType($contract);
-            $employee->setDisability($disability);
-            $employee->setDisabilityDesc($disabilityDesc);
-            $employee->setFirstname($fname);
-            $employee->setHireDate($hire_date);
-            $employee->setName($name);
-            $employee->setPhone($phone);
-            $employee->setSexe($sexe);
-            $employee->setPostalCode($postal);
-            $employee->setSocialSecurityNumber($secuNum);
-            $employee->setRole($roleEmployee);
-
-            $user->setEmail($email);
-            $user->setUsername($username);
-            $user->setRoles($roles);
-            $user->setEnabled($enabled);
-            $user->setInvitationToken($token);
-            $user->setInvitationTokenExpiry($expiry);
-            $user->setCreatedAt($created_at);
-            $em->persist($user);
-            $em->persist($employee);
-            $em->flush();
-
-
+                $user->setEmail($email);
+                $user->setUsername($username);
+                $user->setRoles($roles);
+                $user->setEnabled($enabled);
+                $user->setInvitationToken($token);
+                $user->setInvitationTokenExpiry($expiry);
+                $user->setCreatedAt($created_at);
+                $em->persist($user);
+                $em->persist($employee);
+                $em->flush();
+            }else {
+               return $this->json(['Erreur :' => 'les données sont absentes']);
+            }
         return $this->json(["tokenUser" => $token]);
     }
 
@@ -163,23 +157,47 @@ class UserController extends AbstractController
     public function verifyToken(Request $request, EntityManagerInterface $em): JsonResponse {
         $decode = json_decode($request->getContent());
         $token =  $decode->token;
-
         $user = $em->getRepository(User::class)->findOneBy(['invitationToken' => $token]);
-
         if(empty($user)) {
-            return $this->json(['error']);
+            return $this->json([$token]);
         }
-
 
         return $this->json(['valid' => true]);
     }
 
-    #[Route('/test', name: 'test', methods: "GET")]
-    public function test(Request $request, EntityManagerInterface $em): JsonResponse {
+    //#[Route('/test', name: 'test', methods: "GET")]
+    //public function test(Request $request, EntityManagerInterface $em): JsonResponse
+    //{
+        //return $this->json(['valid' => true]);
+    //}
+    #[Route('/setpassword', name: 'setpassword', methods: "POST")]
+    public function setPassword(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordEncoder): JsonResponse
+    {
+        $decode = json_decode($request->getContent());
+        $token = $decode->token;
+        $user = $em->getRepository(User::class)->findOneBy(['invitationToken' => $token]);
+        if (empty($user)) {
+            return $this->json(['Error :' => 'aucune donnée user']);
+        }
+        if (empty($decode)) {
+            return $this->json(['Error :' => 'aucune donnée']);
+        }
+        if ($user->getEnabled() == 1) {
+            return $this->json(['Error :' => 'compte deja activer']);
+        }elseif ($user->getEnabled() == 0) {
+
+            $password = $decode->password;
+            $hashedPassword = $passwordEncoder->hashPassword($user, $password);
+            $user->setPassword($hashedPassword);
+            $em->persist($user);
+            $em->flush();
 
 
+            $user->setEnabled(1);
+            $user->setInvitationToken(null);
+        }
 
-        return $this->json(['valid' => true]);
+        return $this->json(['TOKEN' => $decode->token,'pass' => $decode->password, 'user' => $user->getEmail()]);
     }
 
 
